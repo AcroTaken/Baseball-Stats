0c82bb9b511186f08775583b38173fb38bbd5c5b
Sven Nguyen <svennguyen@Svens-MacBook-Pro.local>
2025-11-20 20:46:13 -0600

    fixed node_modules

--- Baseball-Stats/HTML/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-const { Pool } = require("pg");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
--- Baseball-Stats/sql/05_indexes.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX ix_batting_team_year ON Batting (teamID, yearID);
-CREATE INDEX ix_pitching_team_year ON Pitching (teamID, yearID);
-CREATE INDEX ix_appearances_team_year ON Appearances (teamID, yearID);
